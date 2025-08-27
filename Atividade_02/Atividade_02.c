41ba95115adac4bfcd8d73601196d167712a5b29
HELTEX <10742524@mackenzista.com.br>
2025-08-27 11:50:10 +0000

    atividade02

---
+++ Atividade_02/Atividade_02.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int minutos (int hour, int min) {
+    int total = 0;
+    total = (hour * 60);
+    total = min;
+    return total;
+}
+
+int diferenca (int total1, int total2) {
+    return total1 + total2;
+}
+
+int main() {
+    
+}
